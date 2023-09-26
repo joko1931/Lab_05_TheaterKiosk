d88b366295a9a808cc9ac4e925a28e5dc2dc8784
joko1931 <okoonja@mail.uc.edu>
2023-09-26 17:12:36 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter age: ");
+        int age = in.nextInt();
+
+        if(age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
     }
 }
